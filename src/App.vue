6af8960a50d223947c5a7a2148ad0b3ca2602f18
Chakibdz <54024623+Chakibdz@users.noreply.github.com>
2019-10-23 09:48:06 +0100

    test mac

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
     <div id="app">
         <div id="nav">
-            <router-link to="/">Home</router-link> |
-            <router-link to="/about">About</router-link> |
-            <router-link to="/Help">Help</router-link> |
+            <router-link to="/">Home</router-link>|
+            <router-link to="/about">About</router-link>|
+            <router-link to="/Help">Help</router-link>|
             <router-link to="/NewTest">New</router-link>
         </div>
         <router-view />
